10941e8843c8344c54881945f7723ac4e4c20ef9
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2024-11-25 22:01:02 -0600

    like posts API

--- routes/posts.js
+++ routes/posts.js
@@ -95,8 +95,38 @@ router.delete('/:id',[
     }
 });
 
-router.post('/:id/like', (req, res) => {
-    res.json({ message: 'Post liked API' });
+router.post('/:id/like', [
+    verifyToken,
+], async (req, res) => {
+    const postId = req.params.id;
+
+    if(!ObjectId.isValid(postId)) {
+        return res.status(400).json({message: 'Invalid Post Id'});
+    }
+
+    try {
+        const db = await connectDB();
+
+        const post = await db.collection('posts').findOne({ _id: new ObjectId(String(postId)) });
+        if(!post) {
+            return res.status(404).json({message: 'Post not found'});
+        }
+
+        const result = await db.collection('posts').updateOne(
+            {_id: new ObjectId(String(postId))},
+            { $addToSet: {likes: new ObjectId(String(req.user.userId))} }
+        )
+
+        if(result.modifiedCount === 0) {
+            return res.status(200).json({message: 'You have already liked this post'})
+        }
+        
+        res.status(200).json({message: 'Post liked successfully'});
+    }
+    catch(err) {
+        console.error('Error Liking post:', err)
+        res.status(500).json({message: 'Internal Server Error'})
+    }
 });
 
 export default router;
