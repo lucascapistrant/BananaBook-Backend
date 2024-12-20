a5bafb8dd9ca6a25bc0e2f8cecffa79466603a30
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2024-12-19 21:24:51 -0600

    Add API route to update user biography

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { uploadPhoto, resizeAndUploadPhoto } from "../middleware/imageUploadMiddleware.js";
 import verifyToken from '../middleware/auth.js';
+import { body, validationResult } from "express-validator";
 import connectDB from "../config/db.js";
 import { ObjectId } from "mongodb";
 
@@ -41,6 +42,51 @@ router.put('/:id/profilePicture', uploadPhoto.array('images', 1), [
     } catch(err) {
         console.error("Server Error:", err);
         return res.status(500).json({message: 'Profile picture failed to update'});
+    }
+})
+
+router.put('/:id/bio', [
+    verifyToken,
+    body('biography')
+        .isLength({ min: 1, max: 150 })
+        .withMessage('Biography must be between 1 and 150 characters.')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const db = await connectDB();
+        const id = req.params.id;
+        const user = await db.collection('Users').findOne({_id: new ObjectId(String(id))});
+        const { biography } = req.body;
+    
+        if(!user) {
+            return res.status(404).json({ message: 'User does not exist' });
+        }
+
+        if(id !== req.user.userId) {
+            return res.status(403).json({message: 'You can only update your own biography'});
+        }
+
+        const result = await db.collection('Users').updateOne(
+            {_id: new ObjectId(String(id))},
+            {   $set: {"biography": biography} }
+        )
+
+        if(result.modifiedCount === 0) {
+            return res.status(400).json({message: 'Failed to update user biography or biography is equal to this request'});
+        }
+
+        res.status(200).json({
+            message: 'User biography updated successfully!',
+            biography: biography
+        });
+
+    } catch(err) {
+        console.error("Server Error:", err);
+        return res.status(500).json({message: 'User biography failed to update'});
     }
 })
 
